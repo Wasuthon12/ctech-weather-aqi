b64d5a7ba7a1e52fdfbaf79fbb2e0430f13abd25
Wasuthon12 <6750482@techno-chon.ac.th>
2026-08-23 14:08:32 +0700

    Initial commit

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,28 @@
-self.addEventListener('install', (e) => {
+const CACHE_NAME = 'ctc-weather-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
   self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+// Activate Service Worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+// Fetch Assets
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
